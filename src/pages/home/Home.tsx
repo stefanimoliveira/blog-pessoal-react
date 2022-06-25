0bfb6f37e5d8a16de89f3c2d2d86dd84f1b229f3
unknown <stefanymoliveira87@gmail.com>
2022-06-25 13:23:42 -0300

    criando um projeto com react

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+    <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://s2.glbimg.com/40Z_5QkW0eKy_qaTg20SbdkXgLo=/e.glbimg.com/og/ed/f/original/2019/05/15/58a6130.jpg" alt="Imagem Tela Principal" className="img"/>
+    </>
+    );
+}
+export default Home;
