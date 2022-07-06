16d1f83d0f041b1bad67086edc36a3303de52bb0
unknown <stefanymoliveira87@gmail.com>
2022-07-06 11:04:31 -0300

    conectando tema e postagem com o back-end

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AnyAaaaRecord } from "dns";
 
 export const api = axios.create({
     baseURL:'https://blogpessoalnospring.herokuapp.com/'
@@ -12,4 +13,9 @@ export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
 export const login = async(url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
+}
+
+export const busca = async(url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
 }
