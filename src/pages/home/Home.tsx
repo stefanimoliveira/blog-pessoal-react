d7ff005ee0148c1426226bfcd8a1354951f24ce5
unknown <stefanymoliveira87@gmail.com>
2022-07-07 14:37:06 -0300

    post e put de Postagem )

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {Grid, Typography, Button} from "@material-ui/core";
 import {Box} from "@mui/material";
 import "./Home.css";
 import TabPostagem from "../../components/postagens/tabPostagem/TabPostagem";
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem";
+import { useNavigate } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 
 
 function Home(){
+    
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            navigate("/login")
+
+        }
+    }, [token])
+
     return(
     <>
       <Grid container direction="row" justifyContent="center" alignItems="center" className="caixa">
@@ -16,6 +31,7 @@ function Home(){
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
                         <Button variant="outlined" className="botao">Ver Postagens</Button>
                     </Box>
